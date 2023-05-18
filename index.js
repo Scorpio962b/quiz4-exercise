e312d5ff8afb1abaa9677c13067a9fe6f2d4761e
Scorpio962b <samaaiab962@gmail.com>
2023-05-18 16:26:33 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let car = {
+    color: "blue",
+    speed: "200",
+    model: "polo",
+};
+console.log (car);
+
+let car2 = {
+    color: "grey",
+    speed: "350",
+    model: "bmw",
+};
+console.log (car2);
+
+let moter= [ "car" , "car2" ];
+
+console.log (moter);
